56ce1ff1061755945ddd2cee87939fc393acf1a3
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2025-12-05 00:27:44 -0600

    fixed serial communication issue with arduino

--- main_code.py
+++ main_code.py
@@ -1,5 +1,6 @@
 # Coded by Nathan
 
+
 import serial
 import pynmea2
 import time
@@ -7,30 +8,37 @@ import csv
 import psutil as ps
 import os
 
+
 current_latitude = "N/A"
 current_longitude = "N/A"
 current_moisture = "N/A"
 sample_ID = 0
 
+
 serMoisture = None
 serGps = None
 
 
-def communicate_to_actuator():
+
+
+def communicate_to_actuator(command):
+    global serMoisture
+    cmd_char = command[0]
+    encode_to_bytes = (cmd_char+ '\n').encode('utf-8')
     try:
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        time.sleep(2)
+        serMoisture.write(encode_to_bytes)
+        if cmd_char == "e":
+            print("Actuator extended.")
+        elif cmd_char == "r":
+            print("Actuator retracted.")
+        elif cmd_char == 'l':
+            print("Sensor triggered.")
     except serial.SerialException as e:
-        print("Could not communicate with arduino.")
+        print(f"Error sending command to Arduino: {e}")
     except Exception as e:
-        print("Error")
-    encode_to_bytes = (command + '\n').encode('utf-8')
-    ser.write(encode_to_bytes)
-    if command == "extend":
-        print("Actuator extended.")
-    elif command == "retract":
-        print("Actuator retracted.")
-        time.sleep(5)
+        print(f"Error: {e}")
+
+
 
 
 def initialize_log_file(filename="data.csv"):
@@ -39,30 +47,35 @@ def initialize_log_file(filename="data.csv"):
             file.write('Sample ID, Latitude, Longitude, Moisture\n')
 
 
+
+
 def update_moisture_reading():
-    time.sleep(5)
+    time.sleep(1.5)
     global current_moisture
     if serMoisture.in_waiting > 0:
         try:
-            line1 = serMoisture.readline().decode('utf-8').rstrip()
-            current_moisture = line1
+            line1 = serMoisture.readline().decode('utf-8').replace('\r', '').strip()
+            data = float(line1)
+            current_moisture = data
         except Exception as e:
             print(f"Moisture read error: {e}")
             current_moisture = "Read Error"
-
-
+   
 def update_gps_reading():
     global current_latitude, current_longitude
     if serGps.in_waiting > 0:
         try:
             line2 = serGps.readline().decode('utf-8').rstrip()
 
+
             if line2.startswith('$'):
                 gps_reading = pynmea2.parse(line2)
+
 
                 if hasattr(gps_reading, 'latitude') and hasattr(gps_reading, 'longitude'):
                     current_latitude = str(gps_reading.latitude)
                     current_longitude = str(gps_reading.longitude)
+
 
         except pynmea2.ParseError as e:
             print(f"GPS Parsing error on line: {line2} Error: {e}")
@@ -70,26 +83,36 @@ def update_gps_reading():
             print(f"GPS read error: {e}")
 
 
+
+
 def read_sensor():
     return current_moisture
+
+
 
 
 def read_latitude():
     return current_latitude
 
 
+
+
 def read_longitude():
     return current_longitude
+
+
 
 
 def log_data():
     filename = "data.csv"
 
+
     moisture = read_sensor()
     latitude = read_latitude()
     longitude = read_longitude()
 
-    if latitude != "N/A" or moisture != "N/A":
+
+    if latitude != "N/A" and moisture != "N/A" and type(moisture) is float:
         with open(filename, 'a') as file:
             file.write('{},{},{},{}\n'.format(
                 sample_ID,
@@ -102,6 +125,8 @@ def log_data():
         print("Waiting for valid sensor data...")
 
 
+
+
 if __name__ == '__main__':
     try:
         serMoisture = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -110,32 +135,29 @@ if __name__ == '__main__':
         print(f"FATAL ERROR: Could not open one or more serial ports. Check connections and permissions: {e}")
         exit()
 
+
     serMoisture.flush()
     serGps.flush()
     initialize_log_file()
     print("Starting the program. Press Ctrl+C to stop.")
 
+
     while True:
         command = input("Enter a command: ")
-        if command == "extend":
-            sample_ID += 1
-            communicate_to_actuator()
+        if command == "e":
+            communicate_to_actuator(command)
+        elif command == "r":
+            communicate_to_actuator(command)
+        elif command == "l":
+            communicate_to_actuator(command)
+            global sample_ID
+            sample_ID +=1;
             update_moisture_reading()
             update_gps_reading()
             log_data()
-            time.sleep(1)
-        elif command == "retract":
-            communicate_to_actuator()
-        elif command == "retry sample":
-            print("Retrying data collection...")
-            communicate_to_actuator()
-            update_moisture_reading()
-            update_gps_reading()
-            log_data()
-            time.sleep(1)
+        elif command == "re":
+            sample_ID = 0
         else:
             print("Exiting...")
             break
-
-
 
